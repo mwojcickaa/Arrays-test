032735a59a39b7a015ad4be0703b7b690c1c22fb
mwojcickaa <martynawojcickaaa@gmail.com>
2022-12-02 17:14:05 +0100

    Add loop functions to display list items

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,25 @@
-let buttonAddMe = document.getElementById("add-me")
-let buttonRefreshList = document.getElementById("refresh-list")
+const buttonAddMe = document.getElementById("add-me")
+const buttonRefreshList = document.getElementById("refresh-list")
+const listContents = document.getElementById("list-contents")
+const contentsArray = [];
+let parentList = document.getElementById("list")
+let parent = "<ol>"
+
+function addMe() {
+    let currentText = document.getElementById("current-text").value;
+    if (currentText !== "") {
+        contentsArray.push(currentText)
+        document.getElementById("current-text").value = "";
+    }
+}
+
+function refreshList() {
+    parentList.innerHTML = "";
+    for (i = 0; i <= contentsArray.length - 1; i++) {
+        parent += "<li>" + contentsArray[i] + "</li>";
+    }
+    document.getElementById("list-contents").innerHTML = parent;
+}
+
+buttonAddMe.addEventListener("click", function () { addMe() })
+buttonRefreshList.addEventListener("click", function () { refreshList() })
